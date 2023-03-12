e124ea2c15c854f72044167489c4b9b0fc82f036
varunkaleyra <126595204+varunkaleyra@users.noreply.github.com>
2023-03-12 22:02:32 +0530

    Update setupRemoteMongo.sh

--- setupRemoteMongo.sh
+++ setupRemoteMongo.sh
@@ -1,4 +1,4 @@
 # CRIO_SOLUTION_START_MODULE_DEPLOYMENT
 # CRIO_SOLUTION_END_MODULE_DEPLOYMENT
-mongoimport --uri <add-url-connection-string-here> --drop --collection users --file data/export_qkart_users.json
-mongoimport --uri <add-url-connection-string-here> --drop --collection products --file data/export_qkart_products.json
+mongoimport --uri "mongodb+srv//admin:Pa55word@qkart-node.def5p.mongodb.net/qkart?retryWrites=true&w=majority" --drop --collection users --file data/export_qkart_users.json
+mongoimport --uri "mongodb+srv//admin:Pa55word@qkart-node.def5p.mongodb.net/qkart?retryWrites=true&w=majority" --drop --collection products --file data/export_qkart_products.json
